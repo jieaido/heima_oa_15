ecbdcb24a99c87ce0026b8240c5c9a2129a1f027
Cui-NotePC <jieaido@163.com>
2016-07-04 17:48:05 +0800

    更新BaseController文件,更新modelview文件

---
+++ heima_crm/CRM.Site/Areas/Admin/Models/LoginModelView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRM.Site.Areas.Admin.Models
+{
+    public class LoginModelView
+    {
+        [DisplayName("用户名")]
+        [Required(ErrorMessage = "用户名不许为空")]
+        public string LoginName { get; set; }
+        [DisplayName("密码")]
+        [Required(ErrorMessage = "密码不能为空")]
+        public String LoginPassword { get; set; }
+        [DisplayName("验证码")]
+        [Required(ErrorMessage = "验证码不能为空")]
+        public string Vcode { get; set; }   
+    }
+}
---
+++ heima_crm/CRM.WebHelper/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CRM.IServer;
+
+
+namespace CRM.WebHelper
+{
+    public class BaseController:Controller
+    {
+        protected IsysFunctionServices funSer;
+        protected IsysKeyValueServices keyvalSer;
+        protected IsysMenusServices menuSer;
+        protected IsysOrganStructServices organSer;
+        protected IsysPermissListServices permissSer;
+        protected IsysRoleServices roleSer;
+        protected IsysUserInfoServices userinfoSer;
+        protected IsysUserInfo_RoleServices userinfoRoleSer;
+        protected IwfProcessServices processSer;
+        protected IwfRequestFormServices requestformSer;
+        protected IwfWorkServices workSer;
+        protected IwfWorkBranchServices workbranchSer;
+        protected IwfWorkNodesServices worknodesSer;
+
+    }
+}
