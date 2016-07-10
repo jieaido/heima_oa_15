6abf022c93d76c28a05473515fa048af3bdd8e45
jieaido <jieaido@163.com>
2016-07-10 08:32:31 +0800

    tab的布局完成，需要在 navtab= $("#tab1").ligerTab({                 height: '100%', 设置高度。

---
+++ heima_crm/CRM.Site/Areas/Admin/Controllers/KeyValController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.Site.Areas.Admin.Controllers
+{
+    public class KeyValController : Controller
+    {
+        // GET: Admin/KeyVal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
