b1a11aa167d85d04485781cbe2b04e23340f68e7
Jieaido-t460p <Jieaido@DESKTOP-PT2D06H>
2016-07-17 09:29:46 +0800

    组织部门增加画面小完成。还要修改

--- heima_crm/CRM.Site/Areas/Admin/Controllers/OrganStructController.cs
+++ heima_crm/CRM.Site/Areas/Admin/Controllers/OrganStructController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CRM.IServer;
+using CRM.Model;
 using CRM.WebHelper;
+using CRM.WebHelper.attrs;
 
 namespace CRM.Site.Areas.Admin.Controllers
-{
+{[Skipcheckpermiss]
     public class OrganStructController :BaseController
     {
 
-        public OrganStructController(IsysOrganStructServices organStructServices)
+        public OrganStructController(IsysOrganStructServices organStructServices,IsysKeyValueServices keyValueServices)
         {
             base.organSer = organStructServices;
+            base.keyvalSer = keyValueServices;
         }
         // GET: Admin/OrganStruct
         public ActionResult Index()
@@ -38,6 +41,57 @@ namespace CRM.Site.Areas.Admin.Controllers
 
 
             return Json(new {Rows = result, Total = 0});
+        }
+        [HttpGet]
+        public ActionResult add()
+        {
+            var list = new List<SelectListItem>();
+            foreach (var sysKeyValue in keyvalSer.QueryWhere(k => k.KType == 1))
+            {
+                var tempitem = new SelectListItem()
+                {
+                    Text = sysKeyValue.KName,
+                    Value = sysKeyValue.KID.ToString()
+                };
+                list.Add(tempitem);
+            }
+            if (list.Count>0)
+            {
+                list[0].Selected = true;
+            }
+            ViewData.Add("catelid",list);
+            var listOrgan = new List<SelectListItem>();
+            listOrgan.Add(new SelectListItem() {Text ="顶级",Value = "-1"});
+            foreach (var organ in organSer.QueryWhere(o => true))
+            {
+                var tempitem = new SelectListItem()
+                {
+                    Text = organ.osName,
+                    Value = organ.osID.ToString()
+                };
+                listOrgan.Add(tempitem);
+            }
+            ViewData.Add("pid", listOrgan);
+            
+            return View();
+        }
+        [HttpPost]
+    public ActionResult add(sysOrganStruct model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.osCreatorID = UserManger.GetUserInfoName().uID;
+                model.osCreateTime=DateTime.Now;
+                model.osUpdateID = UserManger.GetUserInfoName().uID;
+                model.osUpdateTime=DateTime.Now;
+                model.osLevel = 1;
+                organSer.AddOrUpdate(model);
+                organSer.SaveChanges();
+                return AjaxSuccess("添加成功！");
+            }
+           
+            return null;
+
         }
     }
 }
